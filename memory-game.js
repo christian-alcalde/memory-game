9542621fd39c79f23d799846a3bb1aa4f0e2cb8e
Christian Alcalde <cc.alcalde@yahoo.com>
2022-03-02 22:37:21 -0800

    fixed shuffling

--- memory-game.js
+++ memory-game.js
@@ -59,7 +59,23 @@ function newGame() {
   }
   scoreTotal = 0;
 
-  createCards(colors);
+  createCards(shuffle(colors));
+}
+
+function shuffle(items) {
+  // This algorithm does a "perfect shuffle", where there won't be any
+  // statistical bias in the shuffle (many naive attempts to shuffle end up not
+  // be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
+  // you're interested, you can learn about it, but it's not important.
+
+  for (let i = items.length - 1; i > 0; i--) {
+    // generate a random index between 0 and i
+    let j = Math.floor(Math.random() * i);
+    // swap item at i <-> item at j
+    [items[i], items[j]] = [items[j], items[i]];
+  }
+
+  return items;
 }
 
 /** Create card for every color in colors (each will appear twice)
@@ -224,19 +240,3 @@ function handleScore() {
 }
 
 /** Shuffle array items in-place and return shuffled array. */
-
-function shuffle(items) {
-  // This algorithm does a "perfect shuffle", where there won't be any
-  // statistical bias in the shuffle (many naive attempts to shuffle end up not
-  // be a fair shuffle). This is called the Fisher-Yates shuffle algorithm; if
-  // you're interested, you can learn about it, but it's not important.
-
-  for (let i = items.length - 1; i > 0; i--) {
-    // generate a random index between 0 and i
-    let j = Math.floor(Math.random() * i);
-    // swap item at i <-> item at j
-    [items[i], items[j]] = [items[j], items[i]];
-  }
-
-  return items;
-}
